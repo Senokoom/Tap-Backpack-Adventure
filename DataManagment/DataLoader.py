58d32f2f4a3a29a4fcbed2b9365ce3a5ee326e48
Lev Kochin <leva.kochin@gmail.com>
2026-04-04 13:06:16 +0300

    Короч, поменял немного DataLoader и ItemGenerator дополнил парочкой функций. Но их надо расписать

--- DataManagment/DataLoader.py
+++ DataManagment/DataLoader.py
@@ -1,14 +1,15 @@
 import yaml
+from pathlib import Path
 
-with open("data/weapons_en_ru.yaml", mode="r", encoding="utf-8") as weapons_data_file:
+project_data_dir = Path(__file__).resolve().parent.parent / "data"
+print(project_data_dir)
+
+
+with open(project_data_dir / "weapons_en_ru.yaml", mode="r", encoding="utf-8") as weapons_data_file:
     weapons_data = yaml.safe_load(weapons_data_file)
 
 language = "ru"
 
 
 def get_weapon_data():
-    return [
-        weapons_data["weapon"],
-        weapons_data["prefix"],
-        weapons_data["suffix"],
-    ]
+    return weapons_data
--- classes/System/ItemGenerator.py
+++ classes/System/ItemGenerator.py
@@ -1,9 +1,34 @@
-from classes.Item.Item import Item
 from classes.Item.Armor import Armor
 from classes.Item.Trinket import Trinket
 from classes.Item.Weapon import Weapon
 from classes.Item.Consumable import Consumable
+from DataManagment.DataLoader import get_weapon_data
+
 
 class ItemGenerator:
+
     def __init__(self):
+        weapon_data = get_weapon_data()
+        self.weapon, self.suffix, self.prefix = weapon_data["weapon"], weapon_data["suffix"], weapon_data["prefix"]
+        self.rarity_weights = {
+            "common": 70,
+            "uncommon": 15,
+            "rare": 10,
+            "epic": 5,
+            "legendary": 1
+        }
+
+
+    #Генерирует СТРОГО оружие
+    def generate_weapon(self) -> Weapon:
+       pass
+
+
+    def create_weighted_lists(self):
         pass
+
+    def apply_weights_bonus(self, bonus):
+       return
+
+
+
